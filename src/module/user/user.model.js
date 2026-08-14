dd55c787099d79cdca657fea562557d72a76bec7
nastaran reshadati <96713994+nastaran-reshadati@users.noreply.github.com>
2026-08-14 10:05:35 +0330

    create user model

---
+++ src/module/user/user.model.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const { Schema, default: mongoose } = require("mongoose");
+
+const OTPSchema = new Schema({
+  code: { type: String, require: false, default: undefined },
+  expiresIn: { type: Number, required: false, default: 0 },
+});
+const userSchema = new Schema(
+  {
+    fullName: { type: String, required: false },
+    mobile: { type: String, required: true, unique: true },
+    otp: { type: OTPSchema },
+    verifiedMobile: { type: Boolean, default: false, required: true },
+  },
+  { timestamps: true },
+);
+
+const userModel = mongoose.model("user", userSchema);
+module.exports = userSchema;
